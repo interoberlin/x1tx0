41075a5aa7c87e30977b07292265e1d231ca5851
Urmel <mail@matthiasbock.net>
2013-06-20 17:18:49 +0000

    works

--- rotgruen.py
+++ rotgruen.py
@@ -4,14 +4,25 @@ import RPi.GPIO as GPIO
 from RPi.GPIO import OUT
 from time import sleep
 
-pin = 31
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, OUT)
 
-for i in range(20):
-	GPIO.output(pin, True)
-	sleep(0.1)
-	GPIO.output(pin, False)
-	sleep(0.1)
+rot = [18, 23, 24, 25, 22]
+gruen = [31, 30, 27, 28, 4]
+
+pins = rot+gruen
+
+for pin in range(len(pins)):
+	GPIO.setup(pins[pin], OUT)
+
+def walk(pins):
+	for pin in range(len(pins)):
+		GPIO.output(pins[pin], True)
+		sleep(0.1)
+		GPIO.output(pins[pin], False)
+
+for i in range(5):
+	walk(rot)
+	walk(rot[::-1])
+	walk(gruen)
+	walk(gruen[::-1])
