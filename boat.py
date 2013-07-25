1422a616b83ef9fa366a01962841f6d14e5981de
root <root@hase.(none)>
2013-07-25 22:11:24 +0000

    pwm works

--- boat.py
+++ boat.py
@@ -1,11 +1,39 @@
 #!/usr/bin/python
 import bobo
+import RPi.GPIO as GPIO
+from RPi.GPIO import OUT
+from time import sleep
 
-@bobo.query('/')
-def index():
-	return open('index.html').read()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 
-@bobo.post('/setxy')
-def lp(bobo_request, text):
-	open('server.log','a').write('test')
-	return bobo.redirect('/')
+throttle = 23
+steering = 24
+GPIO.setup(throttle, OUT)
+GPIO.setup(steering, OUT)
+
+TH = GPIO.PWM(throttle, 1000)
+TH.start(0)
+ST = GPIO.PWM(steering, 1000)
+ST.start(0)
+
+on = False
+
+@bobo.post('/')
+def lp(bobo_request, x=0, y=0):
+	global on
+	if on:
+		return ""
+	on = True
+	try:
+		print str(x)+' - '+str(y)
+		x = 50+int(x)*5
+		y = 50+int(y)*5
+		TH.ChangeDutyCycle(y)
+		ST.ChangeDutyCycle(x)
+	except:
+		print "exception"
+	sleep(0.3)
+	on = False
+	return ""
+
