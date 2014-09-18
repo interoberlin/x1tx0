a01b573adbe9c5ad2abff5c3a29a876697c539fb
Matthias <mail@matthiasbock.net>
2014-09-18 23:38:29 +0200

    sandbox gives rectangle

--- Raspberry Pi/sandbox.c
+++ Raspberry Pi/sandbox.c
@@ -2,27 +2,15 @@
 #include <wiringPi.h>
 
 int main() {
+    wiringPiSetupGpio();
 
-  // Starte die WiringPi-Api (wichtig)
-  if (wiringPiSetupGpio() == -1)
-    return 1;
+    pinMode(23, OUTPUT);
 
-  // Schalte GPIO 17 (=WiringPi Pin 0) auf Ausgang
-  // Wichtig: Hier wird das WiringPi Layout verwendet (Tabelle oben)
-  pinMode(0, OUTPUT);
-
-  // Dauerschleife
-  while(1) {
-    // LED an
-    digitalWrite(0, 1);
-
-    // Warte 100 ms
-    delay(100);
-
-    // LED aus
-    digitalWrite(0, 0);
-
-    // Warte 100 ms
-    delay(100);
-  }
+    while(1)
+    {
+        digitalWrite(23, HIGH);
+        delay(1);
+        digitalWrite(23, LOW);
+        delay(2);
+    }
 }
